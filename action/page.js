32ab2cc35e75b5b9959b8c0dee968211036f5181
Piotr Wilczynski <delwing@gmail.com>
2026-04-27 03:38:26 +0200

    fix action

--- action/page.js
+++ action/page.js
@@ -1,8 +1,14 @@
 const { MudletMapReader } = require("mudlet-map-binary-reader");
+const fs = require("fs");
 
 const inputFile = "map_master3.dat"
 const outputDirectory = "page/data"
 
 let mapModel = MudletMapReader.read(inputFile);
 
-let { mudletMap, colors } = MudletMapReader.export(mapModel, outputDirectory);
+let { mapData, colors } = MudletMapReader.export(mapModel);
+
+fs.writeFileSync(`${outputDirectory}/mapExport.js`, `mapData = ${JSON.stringify(mapData)}`);
+fs.writeFileSync(`${outputDirectory}/mapExport.json`, JSON.stringify(mapData));
+fs.writeFileSync(`${outputDirectory}/colors.js`, `colors = ${JSON.stringify(colors)}`);
+fs.writeFileSync(`${outputDirectory}/colors.json`, JSON.stringify(colors));
